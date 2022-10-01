99fb3393c0250f4dffeaab66554cd301dfe3704b
alt2211 <ruthlessspiritofdarkness@gmail.com>
2022-10-02 00:19:01 +0400

    Добавил окно главного меню директора

---
+++ CourseApp/CourseApp/MainWindowDirectForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CourseApp
+{
+    partial class MainWindowDirectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ButtonBack = new System.Windows.Forms.PictureBox();
+            this.HideButton = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(37)))), ((int)(((byte)(80)))));
+            this.panel2.Controls.Add(this.ButtonBack);
+            this.panel2.Controls.Add(this.HideButton);
+            this.panel2.Controls.Add(this.CloseButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1170, 31);
+            this.panel2.TabIndex = 3;
+            // 
+            // ButtonBack
+            // 
+            this.ButtonBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ButtonBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonBack.Image = global::CourseApp.Properties.Resources.png;
+            this.ButtonBack.Location = new System.Drawing.Point(5, 3);
+            this.ButtonBack.Name = "ButtonBack";
+            this.ButtonBack.Size = new System.Drawing.Size(30, 25);
+            this.ButtonBack.TabIndex = 3;
+            this.ButtonBack.TabStop = false;
+            // 
+            // HideButton
+            // 
+            this.HideButton.AutoSize = true;
+            this.HideButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HideButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.HideButton.ForeColor = System.Drawing.Color.White;
+            this.HideButton.Location = new System.Drawing.Point(1130, 5);
+            this.HideButton.Name = "HideButton";
+            this.HideButton.Size = new System.Drawing.Size(15, 16);
+            this.HideButton.TabIndex = 1;
+            this.HideButton.Text = "_";
+            this.HideButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.CloseButton.ForeColor = System.Drawing.Color.White;
+            this.CloseButton.Location = new System.Drawing.Point(1151, 5);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(16, 16);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "X";
+            this.CloseButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainWindowDirectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(29)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(1170, 630);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainWindowDirectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainWindowDirectForm";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonBack)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox ButtonBack;
+        private System.Windows.Forms.Label HideButton;
+        private System.Windows.Forms.Label CloseButton;
+    }
+}
