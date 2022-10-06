83e8c09ec104f9ea1015e0757fa63583f9935d89
SochnuyFrykt <serebrenikovdanil@gmail.com>
2022-10-06 23:45:47 +0400

    Рефактор кода

--- CourseApp/CourseApp/DBMySQLUtils.cs
+++ CourseApp/CourseApp/DBMySQLUtils.cs
@@ -7,26 +7,20 @@ using MySql.Data.MySqlClient;
 
 namespace SqlConn
 {
-    class DBMySQLUtils
+  class DBMySQLUtils
+  {
+    MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=coursedb;user=sereb");
+
+    public void openConnection()
     {
-        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=coursedb");
-        public void openConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Closed)
-            {
-                connection.Open();
-            }
-        }
-        public void closeConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Open)
-            {
-                connection.Close();
-            }
-        }
-        public MySqlConnection GetConnection()
-        {
-            return connection;
-        }
+      if (connection.State == System.Data.ConnectionState.Closed)
+        connection.Open();
     }
+    public void closeConnection()
+    {
+      if (connection.State == System.Data.ConnectionState.Open)
+        connection.Close();
+    }
+    public MySqlConnection GetConnection() => connection;
+  }
 }
